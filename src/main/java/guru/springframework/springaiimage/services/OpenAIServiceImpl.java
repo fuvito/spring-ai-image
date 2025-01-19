b9cf2258d58a465a21d388164f167735a8a253e6
Fuat Yazar <fyazar@gmail.com>
2025-01-18 22:26:27 -0800

    testing

--- src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
@@ -2,7 +2,12 @@ package guru.springframework.springaiimage.services;
 
 import guru.springframework.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -11,9 +16,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    @Override
-    public Object getImage(Question question) {
+    private final ImageModel imageModel;
 
-       return new Object();
+    @Override
+    public byte[] getImage(Question question) {
+        var options = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1024)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .build();
+
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+
+        var imageResponse = imageModel.call(imagePrompt);
+
+        return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
     }
 }
